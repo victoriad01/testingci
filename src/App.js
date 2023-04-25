9249faee3bf298dd39c7c68bdfd20e3a408a42f7
victoriad01 <94958284+victoriad01@users.noreply.github.com>
2023-04-25 23:31:57 +0100

    added learn more stuff

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React
+          Learn React, GitHub Pages and Git Action.
         </a>
       </header>
     </div>
